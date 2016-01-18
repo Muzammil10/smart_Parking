d49a7925e3baa8ac0b50cd803eb617bd8883d078
Sachavent <ventribout@et.esiea.fr>
2016-01-18 21:09:41 +0100

    Récupère lattitude et longtitude du gps de l'appareil (et non via Google Map)

--- app/src/main/java/org/smartparking/smartparking/AccueilActivity.java
+++ app/src/main/java/org/smartparking/smartparking/AccueilActivity.java
@@ -1,12 +1,16 @@
 package org.smartparking.smartparking;
 
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.ParseUser;
@@ -15,6 +19,9 @@ public class AccueilActivity extends AppCompatActivity {
 
     public Button launchgoogleView;
     public Button saveplaceView;
+    private LocationManager locationManager;
+    private double latitude;
+    private double longtitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,7 @@ public class AccueilActivity extends AppCompatActivity {
 
         launchgoogleView= (Button) findViewById(R.id.btn_launch_map);
         saveplaceView=(Button) findViewById(R.id.btn_saveplace);
+
     }
 
     @Override
@@ -73,6 +81,21 @@ public class AccueilActivity extends AppCompatActivity {
     // Sauvegarde une place libre sur la position actuelle
     public void btn_sauvegardeplace(View view) {
 
+        // Fonction qui récupère la position actuelle et la stocke dans la base de données
+
+        // Lance le service de localisation
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        // Récupération de la latitude et la longittude
+
+        // ATTENTION IL FAUT GERER LES PERMISSIONS (regarder favori sur gps)
+
+        latitude=locationManager.getLastKnownLocation("gps").getLatitude();
+        longtitude=locationManager.getLastKnownLocation("gps").getLongitude();
+
+
+
         Toast.makeText(getApplicationContext(), "Place Libre Sauvegardée", Toast.LENGTH_LONG).show();
+
     }
 }
